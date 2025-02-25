7862ffad5b065f4cb32491c0c6d083742e946a67
Divyanshu <divydivyanshu319@gmail.com>
2025-02-26 03:04:33 +0530

    renamed

---
+++ React-State/src/App.jsx
@@ -0,0 +1,25 @@
+import { useState } from "react";
+
+function App() {
+  let [counter, setCounter] = useState(12);
+  function addValue() {
+    setCounter(counter + 1);
+  }
+  function removeValue() {
+    if (counter != 0) {
+      setCounter(counter - 1);
+    } else {
+      setCounter(counter);
+    }
+  }
+  return (
+    <>
+      <h1>React</h1>
+      <p>Counter Value = {counter} </p>
+      <button onClick={addValue}>Add Value {counter}</button>
+      <button onClick={removeValue}>Remove Value {counter}</button>
+    </>
+  );
+}
+
+export default App;
