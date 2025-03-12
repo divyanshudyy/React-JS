8a9fda4b059e5449f63ca966244300546355ac9f
Divyanshu <divydivyanshu319@gmail.com>
2025-03-13 00:51:40 +0530

    added style

--- React-Material-UI/src/InfoBox.jsx
+++ React-Material-UI/src/InfoBox.jsx
@@ -3,13 +3,16 @@ import CardContent from "@mui/material/CardContent";
 import CardMedia from "@mui/material/CardMedia";
 import Typography from "@mui/material/Typography";
 
+import AcUnitIcon from "@mui/icons-material/AcUnit";
+import WbSunnyIcon from "@mui/icons-material/WbSunny";
+import ThunderstormIcon from "@mui/icons-material/Thunderstorm";
+
 export default function InfoBox({ weather }) {
-  const HOT_URL =
-    "https://images.unsplash.com/photo-1535304902304-466bec5bf8e7?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D";
-  const COLD_URL =
-    "https://images.unsplash.com/photo-1485962927012-fc4fd446a24d?q=80&w=2032&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D";
-  const RAIN_URL =
-    "https://images.unsplash.com/photo-1623567932970-576132e5d056?q=80&w=1933&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D";
+  const imageUrl = {
+    hot: "https://images.unsplash.com/photo-1535304902304-466bec5bf8e7?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    cold: "https://images.unsplash.com/photo-1485962927012-fc4fd446a24d?q=80&w=2032&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    rain: "https://images.unsplash.com/photo-1623567932970-576132e5d056?q=80&w=1933&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+  };
 
   return (
     <Card sx={{ maxWidth: 345 }}>
@@ -19,15 +22,24 @@ export default function InfoBox({ weather }) {
         height="140"
         image={
           weather.humidity > 80
-            ? RAIN_URL
-            : weather.temp > 25
-            ? HOT_URL
-            : COLD_URL
+            ? imageUrl.rain
+            : weather.temp > 20
+            ? imageUrl.hot
+            : imageUrl.cold
         }
       />
       <CardContent>
         <Typography gutterBottom variant="h5" component="div">
           {weather.city}
+          <i className="px-3">
+            {weather.humidity > 80 ? (
+              <ThunderstormIcon />
+            ) : weather.temp > 25 ? (
+              <WbSunnyIcon />
+            ) : (
+              <AcUnitIcon />
+            )}
+          </i>
         </Typography>
 
         <Typography
